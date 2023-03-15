6873847b17a3061cadb94bf4414245851384e839
Turkana <turkanaceferli@gmail.com>
2023-03-15 23:56:48 +0400

    ShopList Grid View

---
+++ src/components/ShopListItemGrid/ShopListItemGrid.jsx
@@ -0,0 +1,29 @@
+import "./ShopListItemGrid.css";
+import item from "../../images/shopListItem/shopListItem.jpg";
+import { FiShoppingCart, FiHeart } from "react-icons/fi";
+import { FaSearchPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function ShopListItemGrid(props) {
+  return (
+    <div className="ShopListItemGrid">
+      <div className="slig-img">
+        <img alt="item" src={item}></img>
+        <div className="slig-adding">
+          <p><FiShoppingCart className="addIcon cart" /></p>
+          <p><FiHeart className="addIcon heart" /></p>
+          <p><FaSearchPlus className="addIcon serchPlus" /></p>
+        </div>
+      </div>  
+      <div className="slig-info">
+        <div className="slig-title">
+          <p><Link>{props.title}</Link></p>
+        </div>
+        <div className="slig-price">
+          <p>&#8380;{props.price}</p>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
--- src/pages/ShopList/ShopList.jsx
+++ src/pages/ShopList/ShopList.jsx
@@ -1,10 +1,13 @@
 import "./ShopList.css";
 import ShopListItem from "../../components/ShopListItem/ShopListItem";
+import ShopListItemGrid from "../../components/ShopListItemGrid/ShopListItemGrid";
 import PagePath from "../../components/PagePath/PagePath";
 import Brands from "../../components/Brands/Brands";
 import ShopSideBar from "../../components/shopSideBar/shopSideBar";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import { HiViewGrid } from "react-icons/hi";
+import { MdViewList } from "react-icons/md";
 
 export default function ShopList() {
   return (
@@ -17,10 +20,14 @@ export default function ShopList() {
           <p>About 9,620 results</p>
         </div>
         <div className="list-filter">
+          <div className="views">
+              <p className="grid-view"><HiViewGrid/></p>
+              <p className="list-view"><MdViewList/></p>
+          </div>
           <DropdownButton className="dropdown-button" title="Featured">
-            <Dropdown.Item href="#/action-1">Newest Arrivals</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Price: low to high</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Price: high to low</Dropdown.Item>
+            <Dropdown.Item href="#/neweswArrival-1">Newest Arrivals</Dropdown.Item>
+            <Dropdown.Item href="#/lowToHigh-2">Price: low to high</Dropdown.Item>
+            <Dropdown.Item href="#/highTolow-3">Price: high to low</Dropdown.Item>
           </DropdownButton>
         </div>
       </div>
@@ -28,8 +35,41 @@ export default function ShopList() {
         <div className="shopSideBarPart">
         <ShopSideBar/>
         </div>
-        <div className="listProducts">
-          <ShopListItem
+        {/* <div className="listProducts"> */}
+        <div className="gridProducts">
+        <ShopListItemGrid
+            title="Accumsan tincidunt"
+            price="260.00"
+          />
+          <ShopListItemGrid
+            title="Accumsan tincidunt"
+            price="260.00"
+          />
+          <ShopListItemGrid
+            title="Accumsan tincidunt"
+            price="260.00"
+          />
+          <ShopListItemGrid
+            title="Accumsan tincidunt"
+            price="260.00"
+          />
+          <ShopListItemGrid
+            title="Accumsan tincidunt"
+            price="260.00"
+          />
+          <ShopListItemGrid
+            title="Accumsan tincidunt"
+            price="260.00"
+          />
+          <ShopListItemGrid
+            title="Accumsan tincidunt"
+            price="260.00"
+          />
+          <ShopListItemGrid
+            title="Accumsan tincidunt"
+            price="260.00"
+          />
+          {/* <ShopListItem
             title="Accumsan tincidunt"
             price="260.00"
             about="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo."
@@ -53,7 +93,7 @@ export default function ShopList() {
             title="Accumsan tincidunt"
             price="260.00"
             about="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo."
-          />
+          /> */}
         </div>
       </div>
       <Brands />
